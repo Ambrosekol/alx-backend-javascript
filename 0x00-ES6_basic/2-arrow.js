3b31b02255f0f84812d62e1699b42d6e2f43d923
ambrose <kolus7381@gmail.com>
2024-01-02 14:27:35 +0100

    arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,5 +2,10 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    this.addNeighborhood = (newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    this.addNeighborhood = (val) => add(val)
+
+    function add(newNeighborhood) {
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return self.sanFranciscoNeighborhoods;
+      };
   }
