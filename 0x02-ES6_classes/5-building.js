44149d5c4c84af25218c3e26d93dc5936f258d71
ambrose <kolus7381@gmail.com>
2024-01-04 22:52:01 +0100

    constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Invalid type');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw Error('Class extending Building must override evacuationWarningMessage');
   }
 }
