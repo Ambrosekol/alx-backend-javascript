57da36580bac50d3d9a87ea673a357c602213f00
ambrose <kolus7381@gmail.com>
2024-01-09 20:24:41 +0100

    clean Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,20 @@
-export default function cleanSet(set = new Set(), wrd) {
-  return Array.from(set).filter((item) => item.startsWith(wrd)).join('-');
+export default function cleanSet(set, startString) {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
+  }
+
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
